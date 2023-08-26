effff896b304f9ec69bb4216de264e281b613417
Sironi-00 <00sironi@gmail.com>
2023-08-26 02:43:26 +0100

    init nav _ user-memu +

--- jobs-site/src/Components/nav/Nav.js
+++ jobs-site/src/Components/nav/Nav.js
@@ -1,13 +1,12 @@
 import "./Nav.css";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
-function Nav({currentUser = {}}) {
+function Nav({ currentUser = {} }) {
     return (
         <header>
             <div className="avatar">
-            <h1>Goodness Jobs</h1>
-                {currentUser?.username && <p>{currentUser?.username}</p>}
+                <h1>Goodness Jobs</h1>
             </div>
             <nav>
                 <NavLink className="nav-link" to="/">
@@ -23,6 +22,29 @@ function Nav({currentUser = {}}) {
                     Cleaned
                 </NavLink>
             </nav>
+            {currentUser?.authenticated ? (
+                <div className="avatar">
+                    <div id="user-profile">
+                        <img id="profile-img" src="" alt="⚠️" />
+                    </div>
+                    <div className="user-menu">
+                        <p>{currentUser.username}</p>
+                        <Link className="global-links" to="/logout">
+                            Logout
+                        </Link>
+                    </div>
+                </div>
+            ) : (
+                <div>
+                    <Link className="global-links" to="/login">
+                        
+                        Login
+                    </Link>
+                    <Link className="global-links" to="/register">
+                        Register
+                    </Link>
+                </div>
+            )}
         </header>
     );
 }
